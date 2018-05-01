b77224a46f20451d1e1e6f33bf8391376cef7c21
Jack Taylor <msec.nt@gmail.com>
2018-05-01 03:12:10 +0300

    01.05.2018 03:12

--- frontend/Application/Data/View/ViewProvider.jsx
+++ frontend/Application/Data/View/ViewProvider.jsx
@@ -2,11 +2,10 @@ import ModelProvider from "../ModelProvider";
 
 export default class ViewProvider extends ModelProvider {
 	async fetchFields() {
-		// TODO: Implement122324
+		// TODO: Implement this
 	}
 
 	async fetchEntry(id) {
 		return await this.model.FindById(id);
-		// TODO: Implement2224234
 	}
 }
