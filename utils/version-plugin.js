e0bdefa694aed583ac4869e55c6810da84b67c33
Jack Taylor <msec.nt@gmail.com>
2018-05-01 12:12:37 +0300

    01.05.2018 12:12

--- utils/version-plugin.js
+++ utils/version-plugin.js
@@ -1,11 +1,23 @@
 const fs = require('fs');
 const webpack = require(`webpack`);
+const versionDir = __dirname + '/../version';
 
+if(fs.existsSync(versionDir + '/major') == false) {
+	fs.writeFileSync(versionDir + '/major', '1.0');
+}
 
-let major = fs.readFileSync(__dirname + '/../version/major').toString();
-let minor = fs.readFileSync(__dirname + '/../version/minor').toString();
+if(fs.existsSync(versionDir + '/minor') == false) {
+	fs.writeFileSync(versionDir + '/minor', '0');
+}
+
+if(fs.existsSync(versionDir + '/build') == false) {
+	fs.writeFileSync(versionDir + '/build', '0');
+}
+
+let major = fs.readFileSync(versionDir + '/major').toString();
+let minor = fs.readFileSync(versionDir + '/minor').toString();
 minor++;
-fs.writeFileSync(__dirname + '/../version/minor', minor);
+fs.writeFileSync(versionDir + '/minor', minor);
 
 let buildNumber = 1;
 
